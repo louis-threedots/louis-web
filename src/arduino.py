3807e5474071842b070cb8d40ca2de7b5e0772b8
nina-werklaptop <n.becx@flexpulse.com>
2020-04-02 18:43:59 +0200

    implemented contractions in print_text(); added main app settings; added editable settings; added some error interceptions & small improvements;

--- src/arduino.py
+++ src/arduino.py
@@ -2,15 +2,23 @@ import louis_globals as glob
 class Arduino:
 
     def discover(self):
-        glob.mainApp.audio.speak("How many cells would you like to work with?")
-        return int(glob.cust_input())
+        glob.mainApp.audio.speak("How many cells would you like to work with? (integer)")
+        inp = glob.cust_input()
+        try:
+            return int(inp)
+        except:
+            return self.discover()
 
     def run_to_rel_pos(self, rel_angle, cell_index):
         return
 
     def get_pressed_button(self):
-        glob.mainApp.audio.speak("Enter a cell index to imitate a button press.")
-        return int(glob.cust_input())
+        glob.mainApp.audio.speak("Enter a cell index to imitate a button press. (integer)")
+        inp = glob.cust_input()
+        try:
+            return int(inp)
+        except:
+            return self.get_pressed_button()
 
     def ping(self, cell_index):
         return True
